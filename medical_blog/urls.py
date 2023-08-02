647b69194b7d5d2564301cbb0e2f20c71fc4e637
Daria Kowalewa <kowalewa.daria2012@gmail.com>
2023-08-02 16:00:14 +0300

    new

--- medical_blog/urls.py
+++ medical_blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import admin
 from django.urls import path, include, re_path
 from .import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home_view),
     path('about/', views.about_view),
     path('create/', views.create_form_article),
